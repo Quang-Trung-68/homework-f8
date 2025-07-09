9213e82c37dd71ac7dfc0e6e9c2d616b679bb6c8
Quang-Trung-68 <trungsu23@gmail.com>
2025-07-09 11:59:00 +0700

    final react 1.2

--- FinalProjectReact/src/components/RegisterForm/index.jsx
+++ FinalProjectReact/src/components/RegisterForm/index.jsx
@@ -1,6 +1,54 @@
 import { Box, Button, TextField } from "@mui/material";
 import { useState } from "react";
 import { postFormRegister } from "../../fetchApis";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import { useNavigate } from "react-router-dom";
+
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  borderRadius:"10px",
+  boxShadow: 24,
+  p: 2,
+};
+
+function Popup({ open, setOpen }) {
+  // const handleOpen = () => setOpen(true);
+  // const handleClose = () => setOpen(false);
+
+  const navigate = useNavigate();
+
+  const goToLogin = ()=>{
+    navigate("/login")
+  }
+
+  return (
+    <div>
+      <Modal
+        open={open}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Trạng thái đăng ký:
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Đăng ký thành công.
+          </Typography>
+          <Button sx={{m:2}} variant="contained" onClick={goToLogin}>Quay lại trang đăng nhập</Button>
+        </Box>
+      </Modal>
+    </div>
+  );
+}
 
 export const RegisterForm = ({ goToHome }) => {
   const [formRegister, setFormRegister] = useState({
@@ -12,6 +60,8 @@ export const RegisterForm = ({ goToHome }) => {
     status: "confirmed",
   });
 
+  const [open, setOpen] = useState(false);
+
   const onChange = (name, value) => {
     setFormRegister({ ...formRegister, [name]: value });
     console.log(formRegister);
@@ -21,6 +71,8 @@ export const RegisterForm = ({ goToHome }) => {
     try {
       const data = await postFormRegister({ ...formRegister });
       console.log(data);
+      setOpen(true);
+      console.log(open);
     } catch (error) {
       console.log(error);
     }
@@ -28,6 +80,7 @@ export const RegisterForm = ({ goToHome }) => {
 
   return (
     <div style={{ width: "50vw" }}>
+      <Popup open={open} setOpen={setOpen} />
       <Box>
         <p>Tên của bạn</p>
         <TextField
@@ -88,7 +141,11 @@ export const RegisterForm = ({ goToHome }) => {
         >
           Hủy
         </Button>
-        <Button sx={{ fontWeight: "bold", m: 3 }} variant="contained" onClick={postForm} >
+        <Button
+          sx={{ fontWeight: "bold", m: 3 }}
+          variant="contained"
+          onClick={postForm}
+        >
           Đăng ký
         </Button>
       </div>
